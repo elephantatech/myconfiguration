262af6dcbc179527255f4a9881445b9610dbd62d
elephantatech <vivekm.mistry@gmail.com>
2022-03-17 16:41:00 -0400

    fixed typo on script

--- tools/mongodb/run_mongo.sh
+++ tools/mongodb/run_mongo.sh
@@ -7,8 +7,8 @@
 # download docker image from dockerhub
 docker pull postgres
 # run postgres docker container
-docker run --name postgres \
+docker run --name mongodb \
      -e MONGO_INITDB_ROOT_USERNAME=root \
      -e MONGO_INITDB_ROOT_PASSWORD=password \
-     -p "5432:5432" \
-     -d 27017:27017
+     -p 27017:27017 \
+     -d mongo
